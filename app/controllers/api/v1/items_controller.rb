479c9d19f07f0665951a330156db79483c620427
Chau Tran <cctran96@gmail.com>
2022-08-26 15:26:42 -0700

    methods created for cateogries and items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
+    skip_before_action :authorized
+
+    def index
+        items = Item.all
+        render json: items, status: :accepted
+    end
+
 end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::OrdersController < ApplicationController
+
+    def user_order
+        orders = Order.find_by(user_id: @current_user.id)
+        render json: {orders: orders}, status: :accepted
+    end
 end
