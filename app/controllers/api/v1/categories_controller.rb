55d4674ae9811d06eb838caa484cf09b1743cc4b
Harmandeep Kaur <harmandeepk628@gmail.com>
2022-08-26 15:18:58 -0700

    created method for category controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::CategoriesController < ApplicationController
+
+    skip_before_action :authorized
+
+    def index
+        categories = Category.all
+        render json: categories, status: :accepted
+    end
+
 end
